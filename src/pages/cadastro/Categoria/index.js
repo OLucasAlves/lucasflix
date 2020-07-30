23c44b14fee38e5f8ed36d1b4df84745efa18a66
Lucas Ribeiro <lucas.alves6540@gmail.com>
2020-07-30 13:00:23 -0300

    Alterando server.js de local e inserindo url da api do heroku

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -36,19 +36,19 @@ function CadastroCategoria() {
   }
 
    useEffect(() => {
-    if(window.location.href.includes('localhost')) {
-      const URL = 'http://localhost:8080/categorias'; 
-      fetch(URL)
-       .then(async (respostaDoServer) =>{
-        if(respostaDoServer.ok) {
-          const resposta = await respostaDoServer.json();
-          setCategorias(resposta);
-          return; 
-        }
-        throw new Error('Não foi possível pegar os dados');
-       })
-    }    
-  }, []);
+    const URL = window.location.hostname.includes('localhost')
+    ? 'http://localhost:8080/categorias'
+    : 'http://lucasflix-api.herokuapp.com/';
+  fetch(URL)
+    .then(async (response) => {
+      if (response.ok) {
+        const result = await response.json();
+        setCategorias(result);
+        return;
+      }
+      throw new Error('Não foi possível pegar os dados');
+    });
+}, []);
   return (
     <PageDefault>
       <div >
